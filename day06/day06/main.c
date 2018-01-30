7de709b1fc964bc10f7517112b928101b687ddb5
ChenYXIN <donkor@yeah.net>
2018-01-30 23:30:49 +0800

    day 06

--- day06/day06/main.c
+++ day06/day06/main.c
@@ -1,16 +1,57 @@
 //
 //  main.c
-//  循环的应用场景
+//  循环应用之计算与穷举
 //
 //  Created by Donkor on 2018/1/28.
 //  Copyright © 2018年 Donkor. All rights reserved.
 //
 
 #include <stdio.h>
-
+//#include <stdlib.h>
 int main(int argc, const char * argv[]) {
+
     
     
+//    int num = 2;
+//    while (1) {
+//        if(num % 2 == 1 && num % 3 == 1 && num % 4 ==1){
+//            printf("%d\n",num);
+//            break;
+//        }
+//        num++;
+//    }
+    
+//    int num = arc4random_uniform(100) + 1;
+//    printf("%d\n",num);
+//    int i = 1;
+//    while (1) {
+//        if (num == i){
+//            printf("%d\n",i);
+//            break;
+//        }
+//        i++;
+//    }
+    
+//    int i = 1,count = 0;
+//    while (i <= 100) {
+//        if(i % 3 == 0){
+//            count++;
+//        }
+//        i++;
+//    }
+//    printf("%d\n",count);
+    
+    
+//    int zhanghao = 0 ,mima = 0;
+//    int times = 0;
+//    while (zhanghao != 123456 || mima != 888) {
+//        printf("请输入账号:\n");
+//        scanf("%d",&zhanghao);
+//        printf("请输入密码：\n");
+//        scanf("%d",&mima);
+//        times++;
+//    }
+//    printf("你一个输入了多少次%d,登陆成功!\n",times);
     
     return 0;
 }
