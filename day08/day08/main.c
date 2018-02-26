c37f7c614d3ec84f8a66081454b7db3282fec0de
ChenYXIN <donkor@yeah.net>
2018-02-26 22:32:02 +0800

    day 08

---
+++ day08/day08/main.c
@@ -0,0 +1,28 @@
+//
+//  main.c
+//  预处理指令、预处理代码
+//  以#开头的代码就叫预处理代码
+/*
+    预处理指令
+        1）预处理指令的分类
+            a.文件包含指令 #include
+            b.宏定义 #define
+            c.条件编译指令  #if
+        2)预处理指令的特点
+            a.都是以#开头
+            b.预处理指令的后面没有分号
+            c.在编译的时候，检查语法之前
+ */
+//
+//  Created by Donkor on 2018/2/26.
+//  Copyright © 2018年 Donkor. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    
+    
+    return 0;
+}
